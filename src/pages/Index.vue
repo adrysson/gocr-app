55f53f51e3097437a34d5612220a91091fa345c7
Adrysson <adrysson@doity.com.br>
2021-03-28 17:51:32 -0300

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-uploader
+      url="http://192.168.0.23:3000"
+      label="Send your image / PDF"
+      :headers="headers"
+      field-name="pdf"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  methods: {
+    headers () {
+      return [{name: 'Content-Type', value: 'multipart/form-data'}]
+    }
+  }
+}
+</script>
